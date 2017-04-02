51bb267545d0478455964f5c5c136df6a5970e48
leixinxin <leixinxin@leying365.com>
2017-04-02 15:27:28 +0800

    fetch

--- reddit_api/components/AsyncRoot.js
+++ reddit_api/components/AsyncRoot.js
@@ -2,15 +2,23 @@ import React, { Component, PropTypes } from 'react'
 
 import { selectSubreddit, fetchPostsIfNeeded, invalidateSubreddit } from '../actions'
 
+import Picker from './Picker'
+import Posts from './Posts'
+
 class AsyncRoot extends Component {
 
     constructor(props) {
         super(props)
         this.handleChange = this.handleChange.bind(this)
+        this.handleRefresh = this.handleRefresh.bind(this)
     }
 
     handleChange(nextSubreddit) {
         this.props.dispatch(selectSubreddit(nextSubreddit))
+    }
+
+    handleRefresh() {
+
     }
 
     render () {
@@ -22,6 +30,24 @@ class AsyncRoot extends Component {
                         onChange={this.handleChange}
                         options={['reactjs','frontend']}
                 />
+                {lastUpdated &&
+                    <h2>last update time: {new Date(lastUpdated).toLocaleTimeString()}. {' '}</h2>
+                }
+                {!isFetching &&
+                    <a href="#" onClick={this.handleRefresh}>refresh</a>
+                }
+                {isFetching && posts.length === 0 &&
+                    <h2>loading...</h2>
+                }
+                {!isFetching && posts.length === 0 &&
+                    <h2>this topic is empty</h2>
+                }
+                {posts.length > 0 &&
+                    <Posts
+                        {isFetching}
+                        {posts}
+                    />
+                }
             </div>
         )
     }
